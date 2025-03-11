3215d21655899501733984af87e3aa2f2ac8a5e6
Александр Климковецкий <al.klimkovetskiy@gmail.com>
2025-03-11 13:49:54 +0200

    chore: Initial commit

---
+++ src/test/java/com/alkl1m/counter/CounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkl1m.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
